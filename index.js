d1f31c3c0b33833742c60b87d71003fdb1be25f1
dean <deanieschutte@gmail.com>
2025-06-15 07:27:22 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Simple endpoint
+app.get('/hello', (req, res) => {
+    res.json({ message: 'Hello World!' });
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
